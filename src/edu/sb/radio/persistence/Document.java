933d1dd2dc495196b9e5d6d513176a2421ecb00d
huongvu2312 <calm.and.chaotic@gmail.com>
2021-11-18 15:26:11 +0100

    finish ex 1

--- src/edu/sb/radio/persistence/Document.java
+++ src/edu/sb/radio/persistence/Document.java
@@ -2,55 +2,70 @@ package edu.sb.radio.persistence;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
 
+import edu.sb.radio.util.HashCodes;
+
 @Entity(name = "Document")
-@Table(name = "Document")
+@Table(name = "Document", indexes = { @Index(name = "type", columnList = "type", unique = true) })
+@PrimaryKeyJoinColumn(name = "discriminator_id")
 public class Document extends BaseEntity {
-	@Column(nullable = false, name = "hash")
+	private int id;
+	private String hash;
+	private String type;
+	private byte[] content;
+
+	protected Document() {
+		this(HashCodes.sha2HashText(256, "hash"), "some type", "some content".getBytes());
+	}
+
+	public Document(String hash, String type, byte[] content) {
+		this.hash = hash;
+		this.type = type;
+		this.content = content;
+	}
+
+	@Id
+	@Column(name = "documentIdentity")
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Column(nullable = false, name = "hash", unique = true, updatable = true)
 	@Size(min = 64, max = 64)
-    private String hash;
-	
-	@Column(nullable = false, name = "type")
+	public String getHash() {
+		return hash;
+	}
+
+	public void setHash(String hash) {
+		this.hash = hash;
+	}
+
+	@Column(nullable = false, name = "type", updatable = true)
 	@Size(min = 1, max = 63)
-    private String type;
-	
-	@Column(nullable = false, name = "content")
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@Column(nullable = false, name = "content", updatable = true)
 	@Size(min = 1, max = 16777215)
-    private byte[] content;
+	public byte[] getContent() {
+		return content;
+	}
 
-    public String getHash() {
-        return hash;
-    }
-
-    public void setHash(String hash) {
-        this.hash = hash;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-    
-    public byte[] getContent() {
-        return content;
-    }
-
-    public void setContent(byte[] content) {
-        this.content = content;
-    }
-
-    protected Document() {
-    	this("123", "some type", "some content".getBytes());
-    }
-    
-    public Document(String hash, String type, byte[] content) {
-    	this.hash = hash;
-    	this.type = type;
-    	this.content = content;
-    }
+	public void setContent(byte[] content) {
+		this.content = content;
+	}
 }
--- src/edu/sb/radio/persistence/Negotiation.java
+++ src/edu/sb/radio/persistence/Negotiation.java
@@ -1,47 +1,93 @@
 package edu.sb.radio.persistence;
 
 import javax.persistence.Column;
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
+import javax.validation.Valid;
 import javax.validation.constraints.Size;
 
 @Entity(name = "Negotiation")
-@Table(name = "Negotiation")
+@Table(name = "Negotiation", indexes = { @Index(name = "type", columnList = "type", unique = true),
+		@Index(name = "offer", columnList = "offer", unique = true),
+		@Index(name = "answer", columnList = "answer", unique = true) })
+@PrimaryKeyJoinColumn(name = "discriminator_id")
 public class Negotiation extends BaseEntity {
-	@Embedded
-	private NegotiationType negotiationType;
-	
-	@Column(nullable = false, name = "offer")
-	@Size(min = 0, max = 2044)
-    private String offer;
+	public enum NegotiationType {
+		WEB_RTC
+	}
 
-    @Column(name = "answer")
-    @Size(min = 0, max = 2044)
-    private String answer;
+	private int id;
+	private int personID;
+	private NegotiationType type;
+	private String offer;
+	private String answer;
 
-    public String getOffer() {
-        return offer;
-    }
+	protected Negotiation() {
+		this(1, NegotiationType.WEB_RTC, "pls help", null);
+	}
 
-    public void setOffer(String offer) {
-        this.offer = offer;
-    }
+	public Negotiation(int personID, NegotiationType type, String offer, String answer) {
+		this.personID = personID;
+		this.type = type;
+		this.offer = offer;
+		this.answer = answer;
+	}
 
-    public String getAnswer() {
-        return answer;
-    }
+	@Id
+	@Column(name = "negotiationIdentity")
+	public int getId() {
+		return id;
+	}
 
-    public void setAnswer(String answer) {
-        this.answer = answer;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    protected Negotiation() {
-    	this("pls help", null);
-    }
-    
-    public Negotiation(String offer, String answer) {
-    	this.offer = offer;
-    	this.answer = answer;
-    }
+	@ManyToOne
+	@JoinColumn(name = "negotiatorReference", referencedColumnName = "personIdentity", updatable = true)
+	public int getPersonID() {
+		return personID;
+	}
+
+	protected void setPersonID(int personID) {
+		this.personID = personID;
+	}
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, name = "type", updatable = true)
+	@Valid
+	public NegotiationType getType() {
+		return type;
+	}
+
+	public void setType(NegotiationType type) {
+		this.type = type;
+	}
+
+	@Column(nullable = false, name = "offer", updatable = true)
+	@Size(min = 0, max = 2046)
+	public String getOffer() {
+		return offer;
+	}
+
+	public void setOffer(String offer) {
+		this.offer = offer;
+	}
+
+	@Column(nullable = true, name = "answer", updatable = true)
+	@Size(min = 0, max = 2046)
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
 }
